f0757d8548a9af8efdc5549fa0db85a991c79e1b
kim mi seon <miseon920@gmail.com>
2023-09-15 09:47:23 +0900

    'client-router'

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { connectDB } from '@/app/util/database';
 import Link from 'next/link';
+import DetailLink from './DetailLink';
 
 export default async function List() {
     let db = (await connectDB).db('forum');
     let result = await db.collection('post').find().toArray();
-    console.log(result[0])
+    // console.log(result[0])
 
     // array > object 꺼내기
     return (
@@ -13,10 +14,12 @@ export default async function List() {
         {
             result.map((item:{}, idx:number)=>
                 <div className="list-item" key={idx}>
-                    <Link href={`/detail/${result[idx]._id}`}>
+                    <Link href={`/detail/${result[idx]._id}`} prefetch={false}> {/* 게시판의 경우 모든 링크를 다 볼게 아니므로 미리로드할 필요가 없으므로 프리패치를 모두 다 시키면 무리가 있으므로 prefetch={false} 시킴 : 개발중에는 확인 불가하고 배포환경에서 확인가능 */}
+                        {/* object로 나올경우 result[idx]._id.toString() 으로 하여 문자로 변환 시켜야함 */}
                         <h4>{result[idx].title}</h4>
                         <p>1월 1일</p>
                     </Link>
+                    <DetailLink />
                 </div>
             )
             // {return()} 생략가능
