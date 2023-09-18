dde0a37e90874b4f0b7f1499d87681f4dfc167ad
kim mi seon <miseon920@gmail.com>
2023-09-18 10:22:17 +0900

    'write'

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Write() {
   return (
-    <div className='write'>
+    <div className='write p-20'>
         <form action="/api/write" method='POST'>
             <h4>글쓰기</h4>
             {/*action에 url인 /api/api 주소로 요청 보냄, form은 get과 post만 쓸 수 있음 */}
-            <input type="text" name='title' />
-            <textarea name='content'></textarea>
+            <input type="text" name='title' placeholder='글제목' />
+            <textarea name='content' placeholder='글내용'></textarea>
             <button type='submit'>확인</button>
         </form>
     </div>
