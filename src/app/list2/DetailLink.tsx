597b7d17234e4838173df4f6efb27e8a874be1a4
kim mi seon <miseon920@gmail.com>
2023-10-10 13:38:00 +0900

    'cache'

---
+++ src/app/list2/DetailLink.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useParams, usePathname, useRouter, useSearchParams } from 'next/navigation' // next/router가 아니라 next/navigation 임
+import React from 'react'
+
+export default function DetailLink({item}:any) {
+    // useRouter를 이용하여 이동시키기 - 리액트 훅이므로 client 컴포넌트 안에서만 사용가능
+    let router = useRouter();
+    let pathName = usePathname(); // 현재 url 출력
+    let searchParams = useSearchParams(); // 쿼리 스트링 출력
+    let dynamicRoute = useParams(); // 유저가 입력한 다이나믹한 값 출력
+    /*
+        1. 여러페이지를 만들려면 [dynamicRoute]
+        2. 현재 URL 검사 props/useRouter
+        3. 페이지 이동, prefetch useRouter
+    */
+    return (
+        // <button onClick={()=>{ router.push('/')}}>버튼</button>
+        <button onClick={()=>{ router.push(`/update/${item['_id']}`)}}>수정✏️</button>
+        /*
+            router 쓰는경우
+            - 페이지 이동 router.push('이동할 경로');
+            - 뒤로가기 기능 router.back()
+            - 앞으로가기 router.forward()
+            - 새로고침 router.refresh() : 변동이 있는 html 부분만 리프레쉬 시킴 = soft refresh
+            - 페이지 미리 로드 router.prefetch('로드할 경로') : 로드할 경로를 매우빠르게 로드하고 싶다 할때 씀 - 안쓰는 이유는 link 태그만 써도 프리패치됨
+        */
+    )
+}
+
+
+// 클라이언트 컴포넌트 만들기
+// https://nextjs.org/docs/pages/api-reference/functions/use-router
+// 서버컴포넌트에서 리액트 훅을 사용하고 싶을때는 지금처럼 따로 클라이언트 컴포넌트를 만들어서 import하여 사용한다.
--- src/app/list2/page.tsx
+++ src/app/list2/page.tsx
@@ -3,8 +3,8 @@ import { connectDB } from '@/util/database';
 import ListItem from '../components/ListItem';
 
 // 다이나믹 랜더링으로 변경하기
-export const dynamic = 'force-dynamic'; // 이페이지는 다이나믹 페이지로 보여주겠다는 의미
-// export const revalidate = 60; // 이페이지는 60초 지나면 다시 캐싱한다. - 페이지 단위 캐싱
+// export const dynamic = 'force-dynamic'; // 이페이지는 다이나믹 페이지로 보여주겠다는 의미
+export const revalidate = 20; // 이페이지는 60초 지나면 다시 캐싱한다. - 페이지 단위 캐싱
 
 // force-statc = 이페이지를 보여줄때는 스태틱으로 보여주겠다는 의미
 // 작업 후 build -> start를 해서 확인
@@ -18,7 +18,8 @@ export default async function List() {
         await fetch('url', {cache: 'no-store'}) 로 쓰게 되면 매번 서버로 요청해서 새 결과를 가져온다. (실시간 데이터가 중요한경우)
         await fetch('url', {next: {revalidate: 60}'}) 로 쓰게 되면 60초마다 캐싱된 데이터로 갱신해 준다.
         > 위의 디비 결과를 사용할 api로 바꾼 후 fetch로 가져다 쓰면 캐싱기능을 사용 할 수 있다.
-        > fetch로 쓰기 싫다면 revalidate 예약 변수를 쓰면 페이지 단위로 캐싱이 가능하다.
+        > fetch로 쓰기 싫다면 revalidate 예약 변수를 쓰면 페이지 단위로 캐싱이 가능하다. - 이전에는 이부분을 ISR이라고 불렀음
+        > server component 안에서만 캐싱기능 사용가능
     */
 
     // console.log(result[0])
